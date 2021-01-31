ee58064addd0b61e00d72b1f9d7a9d169ae61ac8
MDmubarak786 <mohammedmubarakmk@gmail.com>
2021-01-31 10:29:33 +0530

    Whatsapp-clone-marvel-version

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "WhatsApp",
+      title: "WhatsApp-Clone",
       theme: new ThemeData(
         primaryColor: new Color(0xff075E54),
         accentColor: new Color(0xff25D366),
